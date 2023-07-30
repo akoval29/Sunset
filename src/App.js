bb849fcf082ba0a075740e9278353cf6540a8d4f
Andriy Koval <akoval29@gmail.com>
2023-07-30 08:49:26 +0300

    got rid of related links

--- src/App.js
+++ src/App.js
@@ -55,54 +55,38 @@ export const App = () => {
             <Route path="/todos" element={<Todos />} />
             <Route path="/users" element={<Users />} />
           </Routes>
-
-          <aside className="app__links">
-            <h3 className="app__links-title">Related links</h3>
-
-            <a className="app__text-link" href="https://www.facebook.com/">
-              facebook
-            </a>
-            <a className="app__text-link" href="https://twitter.com/">
-              twitter
-            </a>
-            <a className="app__text-link" href="https://www.youtube.com/">
-              youtube
-            </a>
-            <a className="app__text-link" href="https://www.instagram.com/">
-              instagram
-            </a>
-
-            <a className="app__img-link" href="https://www.facebook.com/">
-              <img
-                className="app__link-icon"
-                src="https://cdn-icons-png.flaticon.com/512/254/254390.png"
-                alt="img-facebook"
-              />
-            </a>
-            <a className="app__img-link" href="https://twitter.com/">
-              <img
-                className="app__link-icon"
-                src="https://cdn-icons-png.flaticon.com/512/25/25347.png"
-                alt="img-twitter"
-              />
-            </a>
-            <a className="app__img-link" href="https://www.youtube.com/">
-              <img
-                className="app__link-icon"
-                src="https://cdn-icons-png.flaticon.com/512/3938/3938035.png"
-                alt="icon-youtube"
-              />
-            </a>
-            <a className="app__img-link" href="https://www.instagram.com/">
-              <img
-                className="app__link-icon"
-                src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png"
-                alt="icon-instagram"
-              />
-            </a>
-          </aside>
         </section>
-        <footer className="app__footer">Footer</footer>
+
+        <footer className="app__footer">
+          <a href="https://www.facebook.com/">
+            <img
+              className="app__footer-icon"
+              src="https://cdn-icons-png.flaticon.com/512/254/254390.png"
+              alt="img-facebook"
+            />
+          </a>
+          <a href="https://twitter.com/">
+            <img
+              className="app__footer-icon"
+              src="https://cdn-icons-png.flaticon.com/512/25/25347.png"
+              alt="img-twitter"
+            />
+          </a>
+          <a href="https://www.youtube.com/">
+            <img
+              className="app__footer-icon"
+              src="https://cdn-icons-png.flaticon.com/512/3938/3938035.png"
+              alt="icon-youtube"
+            />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img
+              className="app__footer-icon"
+              src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png"
+              alt="icon-instagram"
+            />
+          </a>
+        </footer>
       </article>
     </Router>
   );
