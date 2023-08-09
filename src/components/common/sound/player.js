881813af5b99f4ba62693c25194e573f42e38537
Andriy Koval <akoval29@gmail.com>
2023-08-09 07:46:22 +0300

    ref

--- src/components/common/sound/player.js
+++ src/components/common/sound/player.js
@@ -3,17 +3,18 @@ import "./playerStyle.scss";
 
 export const Player = ({ playing, onPlay }) => {
   const [audio] = useState(new Audio());
-  const [volume, setVolume] = useState(0.4);
+  const [volume, setVolume] = useState(0.7);
 
   const playList = [
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%92%D0%B5%D1%82%D0%B5%D1%80.mp3",
+    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9C%D0%B5%D1%87%D1%82%D0%B0.mp3",
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%9F%D0%B5%D1%89%D0%B5%D1%80%D0%BD%D1%8B%D0%B9%20%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%20%D0%98%D0%BD%D0%BA%D0%B5%D1%80%D0%BC%D0%B0%D0%BD.mp3",
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%9F%D0%BE%D0%BB%D0%B5%D1%82%20%D0%BD%D0%B0%20%D0%9C%D0%B5%D1%80%D0%BA%D1%83%D1%80%D0%B8%D0%B9.mp3",
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%A4%D0%BB%D0%B0%D0%BC%D0%B5%D0%BD%D0%BA%D0%BE.mp3",
-    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9C%D0%B5%D1%87%D1%82%D0%B0.mp3",
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9D%D0%B5%D0%BE%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%93%D0%BE%D1%80%D0%BE%D0%B4.mp3",
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9F%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D0%BE%D0%BA.mp3",
     "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9F%D1%83%D1%82%D1%8C%20%D0%94%D0%BE%D0%BC%D0%BE%D0%B9.mp3",
+    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9B%D0%B5%D0%B4%D1%8F%D0%BD%D0%B0%D1%8F%20%D0%BB%D1%83%D0%BD%D0%B0.mp3",
   ];
 
   const play = (source) => {
@@ -47,6 +48,7 @@ export const Player = ({ playing, onPlay }) => {
     return () => {
       audio.removeEventListener("ended", handleAudioEnd);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [audio]);
 
   return (
--- src/components/pages/main-page/mainPage.js
+++ src/components/pages/main-page/mainPage.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const Main = () => {
-  const localDb = "http://localhost:3001";
-  const fakeApi = "https://jsonplaceholder.typicode.com";
-  const [source, setSource] = useState(localDb);
-
   return (
     <article className="app__main">
       <h3 className="app__main-title">Main</h3>
-      <p className="app__paragraph">Chose your database</p>
+      <p className="app__paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem quo
+        voluptatum consequuntur laudantium expedita obcaecati, repellendus,
+        maiores impedit ducimus tempora earum incidunt, a soluta omnis deserunt
+        explicabo fugiat illo ipsam?
+      </p>
     </article>
   );
 };
