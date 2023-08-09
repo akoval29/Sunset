e0dd908682c8a519b377c8168dcb5715fa78fcf2
Andriy Koval <akoval29@gmail.com>
2023-08-09 21:20:18 +0300

    link switched

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -24,5 +24,5 @@ export const useHttp = () => {
 
 // -------------------- вибери API - розкоментуй потрібне ---------------  //
 
-export const url = "http://localhost:3001"; // локальна БД
-// export const url = "https://jsonplaceholder.typicode.com"; // фейковий API
+// export const url = "http://localhost:3001"; // локальна БД
+export const url = "https://jsonplaceholder.typicode.com"; // фейковий API
