7abf5e40bccb4bbb150e581ac96f345e4a80df4f
Andriy Koval <akoval29@gmail.com>
2023-08-11 12:46:27 +0300

    ref

--- src/components/pages/user-page/userDetails.js
+++ src/components/pages/user-page/userDetails.js
@@ -33,93 +33,94 @@ export const UserDetails = () => {
           }}
           unmountOnExit
         >
-          <div className="detailedUser">
-            <div className="detailedUser__column">
-              <li className="detailedUser__row">
-                <img
-                  className="detailedUser__icon"
-                  src="https://cdn-icons-png.flaticon.com/512/3178/3178158.png"
-                  alt="img-email"
-                />
-                <div className="detailedUser__info">
-                  <div className="detailedUser__info-item">{user.email}</div>
-                  <div className="detailedUser__info-name">email</div>
-                </div>
-              </li>
-
-              <span className="detailedUser__line"></span>
-
-              <li className="detailedUser__row">
-                <img
-                  className="detailedUser__icon"
-                  src="https://cdn-icons-png.flaticon.com/512/455/455705.png"
-                  alt="img-email"
-                />
-                <div className="detailedUser__info">
-                  <div className="detailedUser__info-item">{user.phone}</div>
-                  <div className="detailedUser__info-name">mobile</div>
-                </div>
-              </li>
-
-              <span className="detailedUser__line"></span>
-
-              <li className="detailedUser__row">
-                <img
-                  className="detailedUser__icon"
-                  src="https://img.freepik.com/free-icon/placeholder_318-903608.jpg"
-                  alt="img-email"
-                />
-                <div className="detailedUser__info">
-                  <div className="detailedUser__info-item">
-                    {user.address.city}, {user.address.street},{" "}
-                    {user.address.suite}
+          <>
+            <div className="detailedUser">
+              <div className="detailedUser__column">
+                <li className="detailedUser__row">
+                  <img
+                    className="detailedUser__icon"
+                    src="https://cdn-icons-png.flaticon.com/512/3178/3178158.png"
+                    alt="img-email"
+                  />
+                  <div className="detailedUser__info">
+                    <div className="detailedUser__info-item">{user.email}</div>
+                    <div className="detailedUser__info-name">email</div>
                   </div>
-                  <div className="detailedUser__info-name">address</div>
-                </div>
-              </li>
+                </li>
 
-              <span className="detailedUser__line"></span>
+                <span className="detailedUser__line"></span>
 
-              <li className="detailedUser__row">
-                <img
-                  className="detailedUser__icon"
-                  src="https://icon-library.com/images/icon-website/icon-website-0.jpg"
-                  alt="img-email"
-                />
-                <div className="detailedUser__info">
-                  <div className="detailedUser__info-item">{user.website}</div>
-                  <div className="detailedUser__info-name">website</div>
-                </div>
-              </li>
-
-              <span className="detailedUser__line"></span>
-
-              <li className="detailedUser__row">
-                <img
-                  className="detailedUser__icon"
-                  src="https://static.thenounproject.com/png/509354-200.png"
-                  alt="img-email"
-                />
-                <div className="detailedUser__info">
-                  <div className="detailedUser__info-item">
-                    {user.company.name}
+                <li className="detailedUser__row">
+                  <img
+                    className="detailedUser__icon"
+                    src="https://cdn-icons-png.flaticon.com/512/455/455705.png"
+                    alt="img-email"
+                  />
+                  <div className="detailedUser__info">
+                    <div className="detailedUser__info-item">{user.phone}</div>
+                    <div className="detailedUser__info-name">mobile</div>
                   </div>
-                  <div className="detailedUser__info-name">company</div>
-                </div>
-              </li>
+                </li>
 
-              <span className="detailedUser__line"></span>
+                <span className="detailedUser__line"></span>
 
-              <Link to="/users" className="detailedUser__backBtn">
-                <img
-                  className="detailedUser__icon--back"
-                  src="https://cdn-icons-png.flaticon.com/512/7168/7168657.png"
-                  alt="img-goBack"
-                />
-                Back to Users
-              </Link>
+                <li className="detailedUser__row">
+                  <img
+                    className="detailedUser__icon"
+                    src="https://img.freepik.com/free-icon/placeholder_318-903608.jpg"
+                    alt="img-email"
+                  />
+                  <div className="detailedUser__info">
+                    <div className="detailedUser__info-item">
+                      {user.address.city}, {user.address.street},{" "}
+                      {user.address.suite}
+                    </div>
+                    <div className="detailedUser__info-name">address</div>
+                  </div>
+                </li>
+
+                <span className="detailedUser__line"></span>
+
+                <li className="detailedUser__row">
+                  <img
+                    className="detailedUser__icon"
+                    src="https://icon-library.com/images/icon-website/icon-website-0.jpg"
+                    alt="img-email"
+                  />
+                  <div className="detailedUser__info">
+                    <div className="detailedUser__info-item">
+                      {user.website}
+                    </div>
+                    <div className="detailedUser__info-name">website</div>
+                  </div>
+                </li>
+
+                <span className="detailedUser__line"></span>
+
+                <li className="detailedUser__row">
+                  <img
+                    className="detailedUser__icon"
+                    src="https://static.thenounproject.com/png/509354-200.png"
+                    alt="img-email"
+                  />
+                  <div className="detailedUser__info">
+                    <div className="detailedUser__info-item">
+                      {user.company.name}
+                    </div>
+                    <div className="detailedUser__info-name">company</div>
+                  </div>
+                </li>
+              </div>
             </div>
-          </div>
+            <Link to="/users" className="detailedUser__backBtn">
+              <img
+                className="detailedUser__icon--back"
+                src="https://cdn-icons-png.flaticon.com/512/7168/7168657.png"
+                alt="img-goBack"
+              />
+              Back to Users
+            </Link>
+          </>
         </CSSTransition>
       </TransitionGroup>
     </ul>
