23918692d841d2c1d56658987291378d743a33e4
Andrey Koval <akoval29@gmail.com>
2023-11-22 09:09:55 +0200

    firebase deploy

--- src/components/common/sound/player.js
+++ src/components/common/sound/player.js
@@ -6,11 +6,10 @@ export const Player = ({ playing, onPlay }) => {
   const [volume, setVolume] = useState(0.7);
 
   const playList = [
-    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%92%D0%B5%D1%82%D0%B5%D1%80.mp3",
-    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%A4%D0%BB%D0%B0%D0%BC%D0%B5%D0%BD%D0%BA%D0%BE.mp3",
-    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9D%D0%B5%D0%BE%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%93%D0%BE%D1%80%D0%BE%D0%B4.mp3",
-    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9F%D1%83%D1%82%D1%8C%20%D0%94%D0%BE%D0%BC%D0%BE%D0%B9.mp3",
-    "https://storage.cloud.google.com/sunset2023/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9B%D0%B5%D0%B4%D1%8F%D0%BD%D0%B0%D1%8F%20%D0%BB%D1%83%D0%BD%D0%B0.mp3",
+    "https://firebasestorage.googleapis.com/v0/b/sunset-d77d3.appspot.com/o/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%92%D0%B5%D1%82%D0%B5%D1%80.mp3?alt=media&token=85c882bd-4756-4920-b275-f8a63d597742",
+    "https://firebasestorage.googleapis.com/v0/b/sunset-d77d3.appspot.com/o/%D0%94%D0%B8%D0%94%D1%8E%D0%9B%D1%8F%20-%20%D0%9F%D0%BE%D0%B5%D0%B7%D0%B4%20%D0%BD%D0%B0%20%D0%91%D0%B0%D1%80%D1%81%D0%B5%D0%BB%D0%BE%D0%BD%D1%83.mp3?alt=media&token=719dad24-c5fc-4361-bf72-36b6be5ce96e",
+    "https://firebasestorage.googleapis.com/v0/b/sunset-d77d3.appspot.com/o/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9F%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D0%BE%D0%BA.mp3?alt=media&token=ca8d3d59-9bbc-45dd-ab72-70b742042d7a",
+    "https://firebasestorage.googleapis.com/v0/b/sunset-d77d3.appspot.com/o/%D0%94%D0%B8%D0%B4%D1%8E%D0%BB%D1%8F%20-%20%D0%9F%D1%83%D1%82%D1%8C%20%D0%94%D0%BE%D0%BC%D0%BE%D0%B9.mp3?alt=media&token=6ac943ab-6368-4646-90ec-31cfc134461e",
   ];
 
   const play = (source) => {
